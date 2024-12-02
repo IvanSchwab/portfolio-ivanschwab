a1154a56b4812d2cb2fb1dac1e4fb24546e59b23
Ivan <ivan_schwab@outlook.com>
2024-12-02 17:16:47 -0300

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, 'src'), 
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
